dbef1d88c3c63f2c6267f1b2fc732fe36d4a2bb0
Alexander Niebuhr <alexander@nbhr.io>
2022-09-26 07:43:05 +0200

    feat(psql): add constructor & `query()` (#8)

---
+++ packages/psql/build.ts
@@ -0,0 +1,27 @@
+import { buildSync } from 'esbuild'
+import { builtinModules } from 'node:module'
+import * as pkg from './package.json'
+
+const dependencies = Object.keys(pkg.dependencies)
+const peerDependencies = Object.keys(pkg.peerDependencies)
+const node = `node${pkg.engines.node}`
+
+buildSync({
+	entryPoints: ['src/index.ts'],
+	outfile: 'dist/index.js',
+	minify: true,
+	platform: 'node',
+	// sourcemap: 'inline',
+	target: node,
+	bundle: true,
+	format: 'esm',
+	tsconfig: 'tsconfig.json',
+	sourceRoot: 'src',
+	treeShaking: true,
+	logLevel: 'debug',
+	external: [
+		...builtinModules,
+		...dependencies,
+		...peerDependencies,
+	],
+})
