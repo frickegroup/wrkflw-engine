a90c1474ef6935154fdc5f9f69f9f049a1af3030
Alexander Niebuhr <alexander@nbhr.io>
2022-09-26 11:12:05 +0200

    feat(deepl): add `translate()` executor (#21)

--- packages/deepl/src/index.ts
+++ packages/deepl/src/index.ts
@@ -1,15 +1,27 @@
-// import { Translator } from 'deepl-node'
+import { Translator } from 'deepl-node'
+import type { SourceLanguageCode, TargetLanguageCode } from 'deepl-node'
 
-// export default class Node extends Translator {
-// 	public async execute(): {} {
-// 		const startUsage = await super.getUsage()
-// 		const translationResult = await super.translateText('Hello, World', 'en', 'de', {})
-// 		const endUsage = await super.getUsage()
+export default class Node {
+	#client: Translator
 
-// 		return {
-// 			startUsage,
-// 			translationResult,
-// 			endUsage,
-// 		}
-// 	}
-// }
+	constructor(authKey: string) {
+		this.#client = new Translator(authKey, { maxRetries: 3, minTimeout: 10_000 })
+	}
+
+	public async translate(text: string, opts: {
+		source_language: SourceLanguageCode;
+		target_language: TargetLanguageCode;
+	}) {
+		const startUsage = await this.#client.getUsage()
+		const result = await this.#client.translateText(text, opts.source_language, opts.target_language)
+		const endUsage = await this.#client.getUsage()
+
+		let characterUsage: number | undefined
+		if (endUsage.character && startUsage.character) characterUsage = endUsage.character.count - startUsage.character.count
+
+		return {
+			character_usage: characterUsage,
+			translation: result.text,
+		}
+	}
+}
